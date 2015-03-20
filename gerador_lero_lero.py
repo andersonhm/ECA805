72a0806bfeb402034716c69c3e1e5fd7133088ff
andersonhm <anderson.hmoreira@gmail.com>
2015-03-20 11:19:27 -0300

    Gerador de Lero Lero

---
+++ gerador_lero_lero.py
@@ -0,0 +1,24 @@
+from random import *
+
+coluna1 = ['Caros colegas, ', 'Por outro lado, ', 'Nao podemos esquecer que ', 'Do mesmo modo, ',
+'A pratica mostra que ','Nunca é demais insistir que ','Eh fundamental ressaltar que ',
+'Incentivo ao avanço tecnológico, assim como ', 'Assim mesmo ']
+
+coluna2 = ['a execucao deste projeto ','a complexidade dos estudos efetuados ',
+'a atual estrutura de organizacao ','o novo modelo estrutural aqui preconizado ',
+'o desenvolvimento de formas distintas de atuacao ','a constante divulgacao das informacoes ',
+'a consolidacao das estruturas ','a analise dos diversos resultados ','o inicio do programa de formacao de atitudes ',
+'a expansao de nossa atividade ']
+
+coluna3 = ['nos obriga a analise ','cumpre um papel essencial na formulacao ','auxilia a preparacao e a estruturacao ',
+'contribui para a correta determinacao ','assume importantes posicoes na definicao ','facilita a definicao ',
+'prejudica a percepcao da importancia ','oferece uma boa oportunidade de verificacao ','acarreta um processo de reformulacao ',
+'exige precisao e definicao ']
+
+coluna4 = ['das nossas opcoes de desenvolvimento futuro.','das nossas metas financeiras e administrativas',
+'das atitudes e das atribuicoes da diretoria.','das novas proposicoes.','das opcoes basicas para o sucesso do programa.',
+'do nosso sistema de formacao de quadros.','das condicoes apropriadas para os negocios.','dos indices pretendidos.',
+'das formas de acao.','dos conceitos de participacao geral.']
+
+
+print(choice(coluna1)+choice(coluna2)+choice(coluna3)+choice(coluna4))
